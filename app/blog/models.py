7a727a388057728583db1b78fed9f75aef32993b
Volodymir Zybin <ezhickua@gmail.com>
2023-01-17 17:52:55 +0200

    Add model Post and register adminpanel

--- app/blog/models.py
+++ app/blog/models.py
@@ -1,3 +1,20 @@
+from django.utils import timezone
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+class Post(models.Model):
+    class Meta:
+        verbose_name = 'Create post'
+        verbose_name_plural = "Create post's"
+    title = models.CharField(max_length=256)
+    content = models.TextField(blank=True, null=True)
+    date_create = models.DateTimeField(default=timezone.now)
+    date_update = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
+
--- app/store/admin.py
+++ app/store/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 from .models import Book, Author, Client
+
+
 # Register your models here.
 
-admin.site.register(Book)
-admin.site.register(Author)
-admin.site.register(Client)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['author', 'title', 'amount', 'price', ]
 
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ['name', ]
+
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ['firstname', 'lastname', 'phone', 'email', 'discount', ]
